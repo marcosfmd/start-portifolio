ffb4fb997ba53d18334fdf0befc63a784089b60f
marcosfmd <marcosfelipemaia16@gmail.com>
2022-05-01 22:39:29 -0300

    firts commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function displayAbout () {
+    let arrow = document.getElementById('animation')
+    arrow.classList.toggle('rotate')
+
+    let display = document.querySelector('.about-me')
+    display.classList.toggle('active')
+}
